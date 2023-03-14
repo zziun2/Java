39444d8279bbf29c11cdd6f63359d2057eea3852
Un2 <zzi_u_n2@naver.com>
2023-03-14 20:51:49 +0900

    JAVA 1st Study

---
+++ Chapter1/src/ch04/BinaryTest.java
@@ -0,0 +1,18 @@
+package ch04;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		int num = 10;
+		int bNum = 0B1010; // B는 이진수라는 뜻으로 바이너리를 뜻함, 소문자 가능
+		int oNum = 012; // 0는 8진수라는 뜻
+		int xNum = 0XA; // X는 16진수라는 뜻, 소문자 가능
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(xNum);
+		
+	}
+
+}
