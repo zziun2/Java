2726ad4cb7da9a9fa163c47f4242303749b47ec9
Un2 <zzi_u_n2@naver.com>
2023-05-31 23:24:52 +0900

    text block (java 13 이상)

---
+++ Chapter04/src/ch06/StringTextBlock.java
@@ -0,0 +1,27 @@
+package ch06;
+
+public class StringTextBlock {
+	// java 13 부터 가능
+//	public static void main(String[] args) {
+//		String strBlock = """
+//				This 
+//				is 
+//				text
+//				block
+//				test.""";
+//		System.out.println(strBlock);
+//		
+//		System.out.println(getBlockOfHtml());
+//	}
+//	
+//	public static String getBlockOfHtml() {
+//	    return """
+//	            <html>
+//
+//	                <body>
+//	                    <span>example text</span>
+//	                </body>
+//	            </html>""";
+//	
+//	}
+}
